29fda1edcd643d03da12c41a5dbf31c42d2fd785
Anmol Benipal <124711038+anmolbenipal@users.noreply.github.com>
2023-08-01 18:21:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -34,11 +34,11 @@ const connect = async()=>{
 app.use(express.json());
 app.use(cors(corsOptions));
 app.use(cookieParser());
-app.use('/api/v1/tours',tourRoute);
-app.use('/api/v1/users',userRoute);
-app.use('/api/v1/auth',authRoute);
-app.use('/api/v1/review',reviewRoute);
-app.use('/api/v1/booking',bookingRoute);
+app.use('tour-website-backend.vercel.app/api/v1/tours',tourRoute);
+app.use('tour-website-backend.vercel.app/api/v1/users',userRoute);
+app.use('tour-website-backend.vercel.app/api/v1/auth',authRoute);
+app.use('tour-website-backend.vercel.app/api/v1/review',reviewRoute);
+app.use('tour-website-backend.vercel.app/api/v1/booking',bookingRoute);
 
 
 app.listen(port , ()=>{
