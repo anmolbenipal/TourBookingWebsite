62822dd94ab77877231dce3d06a8bc8b1f56e0d8
Anmol Benipal <124711038+anmolbenipal@users.noreply.github.com>
2023-08-03 21:39:05 +0530

    Update tours.js

--- backend/routes/tours.js
+++ backend/routes/tours.js
@@ -15,13 +15,13 @@ import { verifyAdmin } from "../utils/verifyToken.js";
 const router = express.Router();
 
 //create new tour
-router.post("/", verifyAdmin , createTour);
+router.post("/api/v1/", verifyAdmin , createTour);
 
 //update tour
-router.put("/:id", verifyAdmin , updateTour);
+router.put("/api/v1/:id", verifyAdmin , updateTour);
 
 //delete tour
-router.delete("/:id", verifyAdmin , deleteTour);
+router.delete("/api/v1/:id", verifyAdmin , deleteTour);
 
 //get single tour
 router.get("/:id", getSingleTour);
